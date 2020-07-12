05b21210e2fcc78eed826e845c5f4f190faab710
zhuhq <616928019@qq.com>
2020-07-12 16:24:29 +0800

    🔧build(build): 增加打包脚本

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sum = void 0;
+var sum_1 = __importDefault(require("./sum"));
+exports.sum = sum_1.default;
